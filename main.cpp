d51348433a8da29f3c5ca73cbdb4a3b1ca7f85bf
Shashwat Kashyap <shashwatkashyap@yahoo.in>
2026-06-21 13:16:02 +0530

    Fix: Adding Login Prompts

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@ using namespace std;
 int main()
 {
     string name, password;
-    cin >> name >> password;
+    cout<<""Enter Name : " ;
+    cin >> name;
 
+    cout<<endl;
+    cout<<"Enter password :";
+    cin>> password;
+cout<<endl;
     if (login(name, password)) {
         cout << "Success!" << endl;
     } else {
